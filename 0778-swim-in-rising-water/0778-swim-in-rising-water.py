d1795965f7c345e6ed2b30c4f77ef7c19ba4b9ed
Abir Khan <rawnee63@gmail.com>
2026-07-06 13:21:55 +0530

    Time: 31 ms (94.97%), Space: 12.9 MB (23.12%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,30 +1,25 @@
 import heapq
 
-class Solution:
+class Solution(object):
     def swimInWater(self, grid):
+
         n = len(grid)
 
-        dist = [[float("inf")] * n for _ in range(n)]
-        dist[0][0] = grid[0][0]
+        heap = [(grid[0][0], 0, 0)]
+        visited = set()
+        visited.add((0, 0))
 
-        pq = [(grid[0][0], 0, 0)]   # (time, row, col)
-        dirs = [(1,0), (-1,0), (0,1), (0,-1)]
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
-        while pq:
-            time, r, c = heapq.heappop(pq)
+        while heap:
+            time, x, y = heapq.heappop(heap)
 
-            if (r, c) == (n - 1, n - 1):
+            if x == n - 1 and y == n - 1:
                 return time
 
-            if time > dist[r][c]:
-                continue
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
 
-            for dr, dc in dirs:
-                nr, nc = r + dr, c + dc
-
-                if 0 <= nr < n and 0 <= nc < n:
-                    new_time = max(time, grid[nr][nc])
-
-                    if new_time < dist[nr][nc]:
-                        dist[nr][nc] = new_time
-                        heapq.heappush(pq, (new_time, nr, nc))
+                if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    heapq.heappush(heap, (max(time, grid[nx][ny]), nx, ny))
