ea30a669e9ca82e2c27182ca9505d6ec619d672b
Abir Khan <rawnee63@gmail.com>
2026-08-19 23:22:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.57%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        long long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1;
+
+        while (power > 0) {
+
+            if (power % 2 == 1) {
+                result = result * x;
+            }
+
+            x = x * x;
+            power = power / 2;
+        }
+
+        return result;
+    }
+};
