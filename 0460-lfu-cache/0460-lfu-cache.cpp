c80c7c70950d596d9de4c44c19a897104571134a
Abir Khan <rawnee63@gmail.com>
2026-09-01 20:26:36 +0530

    Time: 131 ms (33.99%), Space: 194.6 MB (31.3%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -2,60 +2,70 @@
 using namespace std;
 
 class LFUCache {
-    int capacity;
-    int time = 0;
+    int capacity, minFreq;
 
-    // key -> {value, frequency, last used time}
-    unordered_map<int, tuple<int, int, int>> mp;
+    // key -> {value, frequency}
+    unordered_map<int, pair<int,int>> mp;
+
+    // frequency -> keys
+    unordered_map<int, list<int>> freq;
+
+    // key -> position
+    unordered_map<int, list<int>::iterator> pos;
+
+    void update(int key) {
+        int f = mp[key].second;
+
+        freq[f].erase(pos[key]);
+
+        if (freq[f].empty() && minFreq == f)
+            minFreq++;
+
+        mp[key].second++;
+
+        freq[f + 1].push_front(key);
+        pos[key] = freq[f + 1].begin();
+    }
 
 public:
     LFUCache(int capacity) {
         this->capacity = capacity;
+        minFreq = 0;
     }
 
     int get(int key) {
         if (!mp.count(key))
             return -1;
 
-        auto &[value, freq, last] = mp[key];
-
-        freq++;
-        last = ++time;
-
-        return value;
+        update(key);
+        return mp[key].first;
     }
 
     void put(int key, int value) {
         if (capacity == 0)
             return;
 
+        // Key already exists
         if (mp.count(key)) {
-            auto &[v, freq, last] = mp[key];
-
-            v = value;
-            freq++;
-            last = ++time;
+            mp[key].first = value;
+            update(key);
             return;
         }
 
+        // Cache full
         if (mp.size() == capacity) {
-            int removeKey = -1;
-            int minFreq = INT_MAX;
-            int oldTime = INT_MAX;
+            int oldKey = freq[minFreq].back();
 
-            for (auto &[k, data] : mp) {
-                auto [v, f, t] = data;
-
-                if (f < minFreq || (f == minFreq && t < oldTime)) {
-                    minFreq = f;
-                    oldTime = t;
-                    removeKey = k;
-                }
-            }
-
-            mp.erase(removeKey);
+            freq[minFreq].pop_back();
+            mp.erase(oldKey);
+            pos.erase(oldKey);
         }
 
-        mp[key] = {value, 1, ++time};
+        // Add new key
+        mp[key] = {value, 1};
+        freq[1].push_front(key);
+        pos[key] = freq[1].begin();
+
+        minFreq = 1;
     }
 };
