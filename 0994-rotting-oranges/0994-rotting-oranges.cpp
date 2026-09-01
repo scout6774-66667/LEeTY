2975e32e51adb5cf995bccadfb316f09237a760e
Abir Khan <rawnee63@gmail.com>
2026-09-01 23:12:45 +0530

    Time: 0 ms (100%), Space: 16.7 MB (69.38%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<int,int>> q;
+        int fresh = 0, time = 0;
+
+        // Put rotten oranges in queue
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2)
+                    q.push({i, j});
+                else if (grid[i][j] == 1)
+                    fresh++;
+            }
+        }
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!q.empty() && fresh > 0) {
+            int size = q.size();
+
+            while (size--) {
+                auto [r, c] = q.front();
+                q.pop();
+
+                for (int k = 0; k < 4; k++) {
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+
+                    if (nr >= 0 && nr < m &&
+                        nc >= 0 && nc < n &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        q.push({nr, nc});
+                    }
+                }
+            }
+
+            time++;
+        }
+
+        if (fresh == 0)
+            return time;
+
+        return -1;
+    }
+};
